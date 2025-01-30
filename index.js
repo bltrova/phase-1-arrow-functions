2cfe9108ea24c9a83fb32118e3c58640ff644cdd
Bruno Trova <bltrova@gmail.com>
2025-01-30 10:51:58 -0800

    arrow-functions-lab

---
+++ index.js
@@ -0,0 +1,16 @@
+// const divide = (a, b) => a / b;
+// divide(2000, 100);
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+
+const square = (x) => {
+    return x ** 2;
+}
+
+
+const add = (a, b) => {
+    return a + b;
+}
